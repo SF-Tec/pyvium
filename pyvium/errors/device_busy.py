9b4e7d58efd192afcb479b7505f5684bf5023cbe
Raúl Marín <rmarin82@gmail.com>
2022-12-07 17:44:20 +0100

    fix(device_busy.py): improve error message

--- pyvium/errors/device_busy.py
+++ pyvium/errors/device_busy.py
@@ -1,5 +1,5 @@
 class DeviceBusyError(Exception):
     '''Raised when the IviumSoft software all devices are busy'''
-    def __init__(self, message = "The selected device is busy, connect/select another device or wait till available."):
+    def __init__(self, message = "The selected device is busy, connect/select another device or wait till it becomes available."):
         self.message = message
         super().__init__(self.message)
