eeca6a711b3f5fcc5dfad864e8a3610f93f22c1a
Raúl Marín <rmarin82@gmail.com>
2022-12-07 15:32:09 +0100

    refactor(get_ivium_dll_path): extract function for retrieving the path of the IviumSoft driver dll to its own file

---
+++ pyvium/util/get_ivium_dll_path.py
@@ -0,0 +1,14 @@
+from os import path
+from sys import maxsize, modules
+
+def get_ivium_dll_path():
+    '''Returns the path to the Ivium driver dll.
+    It will be the 32 or 64 bit veresion depending on the version of Windows.'''
+    module_directory = path.dirname(modules["pyvium"].__file__)
+
+    ivium_dll_filename = "Ivium_remdriver64.dll"
+
+    if maxsize <= 2**32:
+        ivium_dll_filename = "IVIUM_remdriver.dll"
+
+    return path.join(module_directory, ivium_dll_filename)
